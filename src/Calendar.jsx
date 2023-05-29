2dcfae581cbdf8fae81c001b9af8a857d33aa9e8
Milan Marinković <85893400+Marinko23@users.noreply.github.com>
2023-05-29 18:59:39 +0200

    Update Calendar.jsx

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -9,30 +9,24 @@ export default function Calendar() {
   const currentDate = dayjs();
   const [today, setToday] = useState(currentDate);
   const [selectDate, setSelectDate] = useState(currentDate);
-  const btnAdd = document.querySelector(".add");
-  const container = document.querySelector(".post-name-container");
-  const btnNewEvent = document.querySelector(".new-event");
+  const [modalVisible, setModalVisible] = useState(false);
 
-  btnAdd.addEventListener("click", function (e) {
-    e.preventDefault();
-    container.classList.remove("hidden");
-    container.classList.add("flex");
-  });
+  const openModal = () => {
+    setModalVisible(true);
+  };
 
-  btnNewEvent.addEventListener("click", function (e) {
-    e.preventDefault();
-    container.classList.add("hidden");
-    container.classList.remove("flex");
-  });
+  const closeModal = () => {
+    setModalVisible(false);
+  };
   return (
-    <div>
+    <div className="container mx-auto">
       <nav class="mx-0 h-20 flex content-center justify-center m-0 border-b-1 shadow-md">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">
-          <div class="flex items-center justify-center gap-16">
+          <div class="flex items-center justify-center gap-16 sm:justify-start">
             <div class="flex-shrink-0">
               <img src={require("./img/logo.png")} className="w-10 h-15" />
             </div>
-            <div class="hidden md:block">
+            <div class="sm:block">
               <div class="ml-10 flex  space-x-20 items-center">
                 <a
                   href="#"
@@ -64,15 +58,15 @@ export default function Calendar() {
                 <img
                   src={require("./img/add.png")}
                   className="w-10 add cursor-pointer"
-                  onClick={btnAdd}
+                  onClick={openModal}
                 />
               </div>
             </div>
           </div>
         </div>
       </nav>
-      <div className="flex gap-10 sm:divide-x justify-center mx-auto my-0  h-screen w-full items-center sm:flex-col flex-col relative">
-        <div className="w-7/12">
+      <div className="flex flex-col gap-10 sm:divide-x justify-center mx-auto my-0 h-screen w-full items-center sm:flex-col relative">
+        <div className="w-7/12 ">
           <div className="flex justify-center items-center">
             <div
               className="flex gap-10 items-center mb-10
@@ -143,46 +137,56 @@ export default function Calendar() {
             )}
           </div>
         </div>
-        <section className="flex">
-          <form name="oglas" class="post-name-container self-center hidden">
-            <h3>Dodaj novi dogadjaj</h3>
-            <input
-              id="post-name"
-              name="name"
-              placeholder="Ime dogadjaja*"
-            ></input>
+        {modalVisible && (
+          <section className="modal-container">
+            <form name="oglas" class="post-name-container ">
+              <h3>Dodaj novi dogadjaj</h3>
+              <input
+                id="post-name"
+                name="name"
+                placeholder="Ime dogadjaja*"
+              ></input>
 
-            <input
-              id="post-name"
-              name="beleska"
-              placeholder="Dodaj belešku..."
-            ></input>
+              <input
+                id="post-name"
+                name="beleska"
+                placeholder="Dodaj belešku..."
+              ></input>
 
-            <input type="date" id="post-date" />
-            <div className="flex gap-5 justify-center">
-              <input id="post-time" name="vreme" placeholder="Početak"></input>
-              <input id="post-time" name="vreme" placeholder="Kraj"></input>
-            </div>
-            <div className="flex gap-52">
-              <p>Podseti me</p>
-              <label
-                for="check"
-                className="bg-gray-100 cursor-pointer relative w-16 h-8 rounded-full"
+              <input type="date" id="post-date" />
+              <div className="flex gap-5 justify-center">
+                <input
+                  id="post-time"
+                  name="vreme"
+                  placeholder="Početak"
+                ></input>
+                <input id="post-time" name="vreme" placeholder="Kraj"></input>
+              </div>
+              <div className="flex gap-52">
+                <p>Podseti me</p>
+                <label
+                  for="check"
+                  className="bg-gray-100 cursor-pointer relative w-16 h-8 rounded-full"
+                >
+                  <input
+                    type="checkbox"
+                    id="check"
+                    class="sr-only peer"
+                  ></input>
+                  <span class="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:bg-purple-600 peer-checked:left-8 transition-all duration-500"></span>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="bg-purple-600 rounded-lg text-white w-7/12 h-12 new-event"
+                onClick={closeModal}
               >
-                <input type="checkbox" id="check" class="sr-only peer"></input>
-                <span class="w-2/5 h-4/5 bg-white absolute rounded-full left-1 top-1 peer-checked:bg-purple-600 peer-checked:left-8 transition-all duration-500"></span>
-              </label>
-            </div>
-
-            <button
-              type="submit"
-              className="bg-purple-600 rounded-lg text-white w-7/12 h-12 new-event"
-              onClick={btnNewEvent}
-            >
-              <strong>Napravi dogadjaj</strong>
-            </button>
-          </form>
-        </section>
+                <strong>Napravi dogadjaj</strong>
+              </button>
+            </form>
+          </section>
+        )}
       </div>
       <div class="mt-8 flex flex-col gap-4 mb-10">
         <div class="card">
